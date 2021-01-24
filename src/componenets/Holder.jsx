af53068658d7cc2b5aca1a71c29262379fa7071f
Titania01 <adegbitebidmus@gmail.com>
2021-01-24 13:58:43 +0100

    update code base

--- src/componenets/Holder.jsx
+++ src/componenets/Holder.jsx
@@ -4,8 +4,10 @@ function Holder() {
   return (
     <div className="holder">
     <div className="one">
-      <span className="color">WORDPRESS PLUGGINS</span>
+      <span className="color">WORDPRESS PLUGINS</span>
       <span className="color1">8MINS READ </span>
+      <h2 className="five">5 Best WordPress Webinar Plugins for Online Events in 2021</h2>
+      <p className="pee">Video conferencing offers many opportunities due to its near-unlimited attendance potential. <br/> However, it can be challenging to host online events on your own website, which is where <br/> WordPress webinar plugins come in handy.</p>
     </div>
     <div className="two"></div>
     </div>

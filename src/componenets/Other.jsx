ad43ddbfbb0dab53f6d9b2c28e5dfd82872d2305
Titania01 <adegbitebidmus@gmail.com>
2021-01-25 00:32:17 +0100

    update code base

--- src/componenets/Other.jsx
+++ src/componenets/Other.jsx
@@ -8,11 +8,22 @@ const Other = () => {
       <h4>Customer Review</h4>
       <p className="small">How to set them up for WooCommerce </p>    
       </div>
+      <div className="below">
+        <p className="woo">WOOCOMMERCE</p>
+        <h3 className="how">How to Enable Customer <br/> Reviews for WooCommerce</h3>
+        <p className="color1">8 MINS READ </p>
       </div>
+      </div>
+
       <div className="seven">
       <div className="blu">
       <h4>WordPress to HTML</h4>
       <p className="small">Best tools for migrating your website </p>  
+      </div>
+      <div className="below">
+        <p className="woo">TOOLS</p>
+        <h3 className="how">Need a WordPress to HTML  <br/> Converter? Here are the 5 Best <br/> Solutions in 2021 </h3>
+        <p className="color1">7 MINS READ </p>
       </div>
       </div>
       <div className="eight"></div>
